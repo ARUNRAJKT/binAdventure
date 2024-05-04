b17d9bc06af7e361fe0c5957ba917d21ff2965af
Arun Raj K T <arunraj44799@gmail.com>
2024-05-04 12:09:06 +0530

    loader set

--- src/Components/loader.jsx
+++ src/Components/loader.jsx
@@ -3,7 +3,7 @@ import './loader.css'
 export default function loader() {
   return (
     <>
-    <svg>
+    <svg style={{backgroundColor:"#FFFFCC",height:"100vh"}}>
     <text x="50%" y="50%" dy=".35em" text-anchor="middle">
         BIN ADVENTURE
     </text>
